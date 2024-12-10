a58636a46d08094bf4ffc37fef1f52404bad4a80
CPenalba <carolinapenalbacorpa@gmail.com>
2024-12-10 12:51:30 +0100

    Cambio de color

--- FundamentosNetCore/Form1.cs
+++ FundamentosNetCore/Form1.cs
@@ -14,7 +14,7 @@ namespace FundamentosNetCore
             this.txtNombre.Text = "Soy un string";
             this.txtNombre.Width = 220;
             this.txtNombre.TextAlign = HorizontalAlignment.Right;
-            this.btnPulsar.BackColor = Color.Fuchsia; 
+            this.btnPulsar.BackColor = Color.Gold; 
         }
     }
 }
