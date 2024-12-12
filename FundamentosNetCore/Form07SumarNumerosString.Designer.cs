8ba5456e7375fe9e872c253ba542946a2304539d
CPenalba <carolinapenalbacorpa@gmail.com>
2024-12-12 09:29:44 +0100

    Sumar Numeros String

---
+++ FundamentosNetCore/Form07SumarNumerosString.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace FundamentosNetCore
+{
+    partial class Form07SumarNumerosString
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtNumeros = new TextBox();
+            btnSumarNumeros = new Button();
+            lblResultado = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(52, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Introduzca números";
+            // 
+            // txtNumeros
+            // 
+            txtNumeros.Location = new Point(52, 86);
+            txtNumeros.Name = "txtNumeros";
+            txtNumeros.Size = new Size(239, 27);
+            txtNumeros.TabIndex = 1;
+            // 
+            // btnSumarNumeros
+            // 
+            btnSumarNumeros.Location = new Point(56, 151);
+            btnSumarNumeros.Name = "btnSumarNumeros";
+            btnSumarNumeros.Size = new Size(233, 42);
+            btnSumarNumeros.TabIndex = 2;
+            btnSumarNumeros.Text = "Sumar Numeros";
+            btnSumarNumeros.UseVisualStyleBackColor = true;
+            btnSumarNumeros.Click += btnSumarNumeros_Click;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(53, 245);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(92, 20);
+            lblResultado.TabIndex = 3;
+            lblResultado.Text = "lblResultado";
+            // 
+            // Form07SumarNumerosString
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblResultado);
+            Controls.Add(btnSumarNumeros);
+            Controls.Add(txtNumeros);
+            Controls.Add(label1);
+            Name = "Form07SumarNumerosString";
+            Text = "Form07SumarNumerosString";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtNumeros;
+        private Button btnSumarNumeros;
+        private Label lblResultado;
+    }
+}
---
+++ FundamentosNetCore/Form07SumarNumerosString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FundamentosNetCore
+{
+    public partial class Form07SumarNumerosString : Form
+    {
+        public Form07SumarNumerosString()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumarNumeros_Click(object sender, EventArgs e)
+        {
+            bool dato = true;
+            string textoNumeros = this.txtNumeros.Text;
+            int suma = 0;
+            for (int i = 0; i < textoNumeros.Length; i++)
+            {
+                //RECUPERAMOS CADA UNO DE LOS CARACTERES
+                char caracter = textoNumeros[i];
+                //CONVERTIMOS EL CARACATER A NUMERO
+                //CON ESTA CONVERSION ESTA RECUPERANDO EL VALOR ASCII DEL NUMERO
+                //int numero = caracter;
+                //int numero = int.Parse(caracter.ToString());
+                int numero = Convert.ToInt32(caracter.ToString());
+                suma += numero;
+            }
+            this.lblResultado.Text = "La suma es " + suma;
+        }
+    }
+}
