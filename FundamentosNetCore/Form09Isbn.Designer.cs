36c09bf7232feae407bd2600c8799358ea25d5f6
CPenalba <carolinapenalbacorpa@gmail.com>
2024-12-12 10:52:22 +0100

    Validar Isbn

---
+++ FundamentosNetCore/Form09Isbn.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace FundamentosNetCore
+{
+    partial class Form09Isbn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtIsbn = new TextBox();
+            btnValidar = new Button();
+            lblResultado = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Introduzca ISBN";
+            // 
+            // txtIsbn
+            // 
+            txtIsbn.Location = new Point(39, 78);
+            txtIsbn.Name = "txtIsbn";
+            txtIsbn.Size = new Size(304, 27);
+            txtIsbn.TabIndex = 1;
+            // 
+            // btnValidar
+            // 
+            btnValidar.Location = new Point(113, 148);
+            btnValidar.Name = "btnValidar";
+            btnValidar.Size = new Size(162, 32);
+            btnValidar.TabIndex = 2;
+            btnValidar.Text = "Validar ISBN";
+            btnValidar.UseVisualStyleBackColor = true;
+            btnValidar.Click += btnValidar_Click;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(39, 224);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(92, 20);
+            lblResultado.TabIndex = 3;
+            lblResultado.Text = "lblResultado";
+            // 
+            // Form09Isbn
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblResultado);
+            Controls.Add(btnValidar);
+            Controls.Add(txtIsbn);
+            Controls.Add(label1);
+            Name = "Form09Isbn";
+            Text = "Form09Isbn";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtIsbn;
+        private Button btnValidar;
+        private Label lblResultado;
+    }
+}
---
+++ FundamentosNetCore/Form09Isbn.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FundamentosNetCore
+{
+    public partial class Form09Isbn : Form
+    {
+        public Form09Isbn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            string isbn = this.txtIsbn.Text;
+            if (isbn.Length != 10)
+            {
+                this.lblResultado.Text = "El ISBN debe tener 10 caracteres";
+            }
+            bool textIsNumber = true;
+            for (int i = 0; i < isbn.Length; i++)
+            {
+                char caracter = isbn[i];
+                if (char.IsDigit(caracter) == false)
+                {
+                    textIsNumber = false;
+                    break;
+                }
+            }
+            if (textIsNumber == false)
+            {
+                this.lblResultado.Text = "El ISBN debe contener solo números";
+            }
+            else
+            {
+                int suma = 0;
+                for (int i = 0; i < isbn.Length; i++)
+                {
+                    char caracter = isbn[i];
+                    int numero = int.Parse(caracter.ToString());
+                    int multiplicacion = numero * (i + 1);
+                    suma += multiplicacion;
+                }
+                if (suma % 11 == 0)
+                {
+                    this.lblResultado.Text = "ISBN correcto";
+                    this.lblResultado.ForeColor = Color.Blue;
+                }
+                else
+                {
+                    this.lblResultado.Text = "El ISBN introducido no es válido";
+                    this.lblResultado.ForeColor = Color.Red;
+                }
+            }
+        }
+    }
+}
