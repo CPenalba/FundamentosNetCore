9a5061588f9cc486a688c26b3aac34bc5e04e286
CPenalba <carolinapenalbacorpa@gmail.com>
2024-12-17 10:33:10 +0100

    List Delegados

---
+++ FundamentosNetCore/Form17ListDelegados.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FundamentosNetCore
+{
+    partial class Form17ListDelegados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtContador = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // txtContador
+            // 
+            txtContador.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtContador.Location = new Point(56, 37);
+            txtContador.Name = "txtContador";
+            txtContador.Size = new Size(166, 47);
+            txtContador.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(59, 108);
+            button1.Name = "button1";
+            button1.Size = new Size(163, 50);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(270, 108);
+            button2.Name = "button2";
+            button2.Size = new Size(163, 50);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(485, 108);
+            button3.Name = "button3";
+            button3.Size = new Size(163, 50);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(485, 204);
+            button4.Name = "button4";
+            button4.Size = new Size(163, 50);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(270, 204);
+            button5.Name = "button5";
+            button5.Size = new Size(163, 50);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(59, 204);
+            button6.Name = "button6";
+            button6.Size = new Size(163, 50);
+            button6.TabIndex = 6;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // Form17ListDelegados
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(txtContador);
+            Name = "Form17ListDelegados";
+            Text = "Form17ListDelegados";
+            Load += Form17ListDelegados_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtContador;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ FundamentosNetCore/Form17ListDelegados.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FundamentosNetCore
+{
+    public partial class Form17ListDelegados : Form
+    {
+        //DECLARAMOS UNA VARIABLE CONTADOR
+        int contador;
+        List<Button> botones;
+        public Form17ListDelegados()
+        {
+            InitializeComponent();
+            this.contador = 0;
+            this.botones = new List<Button>();
+            //this.button1.Click += BotonPulsado;
+            //SI ALMACENMOS TODOS LOS OBJETOS DENTRI DE LA COLECCION, 
+            //PODEMOS HACER LA ACCION DELEGADA CON UN FOREACH
+            this.botones.Add(button1);
+            this.botones.Add(button2);
+            this.botones.Add(button3);
+            this.botones.Add(button4);
+            foreach (Button boton in this.botones)
+            {
+                boton.Click += BotonPulsado;
+            }
+        }
+
+        //QUIERO QUE AL PULSAR CUALQUIER BOTON EN ESTE METODO CAMBIE DE 
+        //COLOR EL BOTON PULSADO
+
+        void BotonPulsado(object sender, EventArgs e)
+        {
+            this.contador += 1;
+            this.txtContador.Text = this.contador.ToString();
+            //sender es el objeto que realiza la llamada
+            Button miBoton = (Button)sender;
+            miBoton.BackColor = Color.Yellow;
+        }
+
+        private void Form17ListDelegados_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
