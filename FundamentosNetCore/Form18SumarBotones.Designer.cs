93dda0d78476d0a40f55be85ef8d0a6100dd6484
CPenalba <carolinapenalbacorpa@gmail.com>
2024-12-17 11:14:52 +0100

    Sumar botones

---
+++ FundamentosNetCore/Form18SumarBotones.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace FundamentosNetCore
+{
+    partial class Form18SumarBotones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtSuma = new TextBox();
+            btnIniciarApp = new Button();
+            panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Suma";
+            // 
+            // txtSuma
+            // 
+            txtSuma.Location = new Point(106, 51);
+            txtSuma.Name = "txtSuma";
+            txtSuma.Size = new Size(162, 27);
+            txtSuma.TabIndex = 2;
+            // 
+            // btnIniciarApp
+            // 
+            btnIniciarApp.Location = new Point(438, 36);
+            btnIniciarApp.Name = "btnIniciarApp";
+            btnIniciarApp.Size = new Size(260, 56);
+            btnIniciarApp.TabIndex = 3;
+            btnIniciarApp.Text = "Iniciar app";
+            btnIniciarApp.UseVisualStyleBackColor = true;
+            btnIniciarApp.Click += btnIniciarApp_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button13);
+            panel1.Controls.Add(button14);
+            panel1.Controls.Add(button15);
+            panel1.Controls.Add(button16);
+            panel1.Controls.Add(button17);
+            panel1.Controls.Add(button18);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button10);
+            panel1.Controls.Add(button11);
+            panel1.Controls.Add(button12);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(46, 156);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(707, 231);
+            panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(24, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(81, 46);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(141, 25);
+            button2.Name = "button2";
+            button2.Size = new Size(81, 43);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(255, 25);
+            button3.Name = "button3";
+            button3.Size = new Size(81, 43);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(364, 26);
+            button4.Name = "button4";
+            button4.Size = new Size(81, 43);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(471, 26);
+            button5.Name = "button5";
+            button5.Size = new Size(81, 43);
+            button5.TabIndex = 4;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(584, 26);
+            button6.Name = "button6";
+            button6.Size = new Size(81, 43);
+            button6.TabIndex = 5;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(584, 92);
+            button7.Name = "button7";
+            button7.Size = new Size(81, 43);
+            button7.TabIndex = 11;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(471, 92);
+            button8.Name = "button8";
+            button8.Size = new Size(81, 43);
+            button8.TabIndex = 10;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(364, 92);
+            button9.Name = "button9";
+            button9.Size = new Size(81, 43);
+            button9.TabIndex = 9;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(255, 91);
+            button10.Name = "button10";
+            button10.Size = new Size(81, 43);
+            button10.TabIndex = 8;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(141, 91);
+            button11.Name = "button11";
+            button11.Size = new Size(81, 43);
+            button11.TabIndex = 7;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(24, 89);
+            button12.Name = "button12";
+            button12.Size = new Size(81, 46);
+            button12.TabIndex = 6;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(584, 160);
+            button13.Name = "button13";
+            button13.Size = new Size(81, 43);
+            button13.TabIndex = 17;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(471, 160);
+            button14.Name = "button14";
+            button14.Size = new Size(81, 43);
+            button14.TabIndex = 16;
+            button14.Text = "button14";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(364, 160);
+            button15.Name = "button15";
+            button15.Size = new Size(81, 43);
+            button15.TabIndex = 15;
+            button15.Text = "button15";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(255, 159);
+            button16.Name = "button16";
+            button16.Size = new Size(81, 43);
+            button16.TabIndex = 14;
+            button16.Text = "button16";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(141, 159);
+            button17.Name = "button17";
+            button17.Size = new Size(81, 43);
+            button17.TabIndex = 13;
+            button17.Text = "button17";
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(24, 157);
+            button18.Name = "button18";
+            button18.Size = new Size(81, 46);
+            button18.TabIndex = 12;
+            button18.Text = "button18";
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // Form18SumarBotones
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(btnIniciarApp);
+            Controls.Add(txtSuma);
+            Controls.Add(label1);
+            Name = "Form18SumarBotones";
+            Text = "Form18SumarBotones";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtSuma;
+        private Button btnIniciarApp;
+        private Panel panel1;
+        private Button button2;
+        private Button button1;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+    }
+}
