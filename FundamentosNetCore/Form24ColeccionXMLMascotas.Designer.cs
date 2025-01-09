7a5feabbc2a094388b009ca58028804b55e8465f
CPenalba <carolinapenalbacorpa@gmail.com>
2025-01-09 10:46:52 +0100

    Form 24

---
+++ FundamentosNetCore/Form24ColeccionXMLMascotas.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace FundamentosNetCore
+{
+    partial class Form24ColeccionXMLMascotas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtNombre = new TextBox();
+            label2 = new Label();
+            txtRaza = new TextBox();
+            label3 = new Label();
+            txtAnyos = new TextBox();
+            btnNuevaMascota = new Button();
+            label4 = new Label();
+            lstMascotas = new ListBox();
+            btnLeerMascotas = new Button();
+            btnGuardarMascotas = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(54, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(57, 57);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(236, 27);
+            txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(57, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Raza";
+            // 
+            // txtRaza
+            // 
+            txtRaza.Location = new Point(57, 148);
+            txtRaza.Name = "txtRaza";
+            txtRaza.Size = new Size(236, 27);
+            txtRaza.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(57, 212);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Años";
+            // 
+            // txtAnyos
+            // 
+            txtAnyos.Location = new Point(57, 245);
+            txtAnyos.Name = "txtAnyos";
+            txtAnyos.Size = new Size(236, 27);
+            txtAnyos.TabIndex = 5;
+            // 
+            // btnNuevaMascota
+            // 
+            btnNuevaMascota.Location = new Point(57, 317);
+            btnNuevaMascota.Name = "btnNuevaMascota";
+            btnNuevaMascota.Size = new Size(236, 51);
+            btnNuevaMascota.TabIndex = 6;
+            btnNuevaMascota.Text = "Nueva mascota";
+            btnNuevaMascota.UseVisualStyleBackColor = true;
+            btnNuevaMascota.Click += btnNuevaMascota_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(379, 29);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Mascotas";
+            // 
+            // lstMascotas
+            // 
+            lstMascotas.FormattingEnabled = true;
+            lstMascotas.Location = new Point(379, 57);
+            lstMascotas.Name = "lstMascotas";
+            lstMascotas.Size = new Size(172, 304);
+            lstMascotas.TabIndex = 8;
+            lstMascotas.SelectedIndexChanged += lstMascotas_SelectedIndexChanged;
+            // 
+            // btnLeerMascotas
+            // 
+            btnLeerMascotas.Location = new Point(615, 100);
+            btnLeerMascotas.Name = "btnLeerMascotas";
+            btnLeerMascotas.Size = new Size(142, 51);
+            btnLeerMascotas.TabIndex = 9;
+            btnLeerMascotas.Text = "Leer Mascotas";
+            btnLeerMascotas.UseVisualStyleBackColor = true;
+            btnLeerMascotas.Click += btnLeerMascotas_Click;
+            // 
+            // btnGuardarMascotas
+            // 
+            btnGuardarMascotas.Location = new Point(615, 212);
+            btnGuardarMascotas.Name = "btnGuardarMascotas";
+            btnGuardarMascotas.Size = new Size(142, 51);
+            btnGuardarMascotas.TabIndex = 10;
+            btnGuardarMascotas.Text = "Guardar Mascotas";
+            btnGuardarMascotas.UseVisualStyleBackColor = true;
+            btnGuardarMascotas.Click += btnGuardarMascotas_Click;
+            // 
+            // Form24ColeccionXMLMascotas
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnGuardarMascotas);
+            Controls.Add(btnLeerMascotas);
+            Controls.Add(lstMascotas);
+            Controls.Add(label4);
+            Controls.Add(btnNuevaMascota);
+            Controls.Add(txtAnyos);
+            Controls.Add(label3);
+            Controls.Add(txtRaza);
+            Controls.Add(label2);
+            Controls.Add(txtNombre);
+            Controls.Add(label1);
+            Name = "Form24ColeccionXMLMascotas";
+            Text = "Form24ColeccionXMLMascotas";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtNombre;
+        private Label label2;
+        private TextBox txtRaza;
+        private Label label3;
+        private TextBox txtAnyos;
+        private Button btnNuevaMascota;
+        private Label label4;
+        private ListBox lstMascotas;
+        private Button btnLeerMascotas;
+        private Button btnGuardarMascotas;
+    }
+}
